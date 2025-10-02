ba10d0b1501becd22834c559db49bfd9826a0321
Peter Stevenson <54287958+p-stevenson@users.noreply.github.com>
2025-10-02 01:02:02 +0100

    random stars function

--- sketch.js
+++ sketch.js
@@ -1,50 +1,39 @@
-let x;
-let y;
-
-function setup() {
-  createCanvas(400, 400);
-  x = width / 2;
-  y = height / 2;
-  // rectMode(CENTER);
+function setup () {
+  createCanvas (400, 400);
+  frameRate(1)
 }
 
-function draw() {
-  background(220);
-  drawFace(randomWalk());
-  // drawFace(randomWalk(x, y));
-  // drawFace(randomWalk(x, y));
+function draw () {
+  background(30);
+  star2();
+  star2();
+  star2();
+  star2();
+  star2();
+  star2();
+  star2();
+}
+
+function star(x,y,length) {
+  noStroke()
+  fill (255)
+  triangle(x,y - length * 0.25, x - length * 0.5, y + length * 0.5, x + length * 0.5, y + length * 0.5)
+  triangle(x - length * 0.5,y, x + length * 0.5, y, x , y + length * 0.75)
 }
 
 
-function drawFace({x, y}) {
-  strokeWeight(2);
-  fill(255, 255, 0); //
-  ellipse(x, y, random(70,75), random(60,75)); // Face
-  fill(random(0, 255), random(0, 255), random(0, 255));
-  ellipse(x - 10, y - 10, 20, 20); // Left eye
-  ellipse(x + 10, y - 10, 20, random(8, 15)); // Right eye
-  arc(x, y + 10, 20, 20, 0, PI); // Smile
-  fill(0); // Pupil color
-  circle(x - 10, y - 10, 3); // Left pupil
-  circle(x + 10, y - 10, 3); // Right pupil
-}
 
-function randomWalk() {
-  const r = floor(random(4));
-  switch (r) {
-    case 0:
-      x = x + 3;
-      break;
-    case 1:
-      x = x - 3;
-      break;
-    case 2:
-      y = y + 3;
-      break;
-    case 3:
-      y = y - 3;
-      break;
-  }
-  return {x, y};
+function star2 () {
+  noStroke();
+  let starSize = random(4, 20)
+  let centerX = random(20, 380);
+  let centerY = random(0, 150);
+  let centerYOffset = centerY - starSize * 1.45;
+  fill(random(200, 220), random(230, 240), random(240, 255));
+  triangle(centerX - starSize, centerY - starSize, 
+            centerX + starSize, centerY - starSize, 
+            centerX, centerY);
+  triangle(centerX  + starSize, centerYOffset + starSize, 
+            centerX - starSize, centerYOffset + starSize, 
+            centerX, centerYOffset);
 }
-
