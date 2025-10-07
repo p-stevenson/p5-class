3028cb45f1322b3c5192bb9dc6b6de8b57c4b5c4
Peter Stevenson <54287958+p-stevenson@users.noreply.github.com>
2025-10-07 11:06:18 +0100

    add directional probabilities

--- sketch.js
+++ sketch.js
@@ -4,11 +4,19 @@ function setup() {
   createCanvas(400, 400);
   background(220);
   walker = new Walker();
+  walker2 = new Walker();
+  walker3 = new Walker();
+  walker4 = new Walker();
 }
 
 function draw() {
   walker.step();
   walker.show();
+  walker.sing();
+  walker2.step();
+  walker2.show();
+  walker3.step();
+  walker3.show();
 }
 
 class Walker {
@@ -17,22 +25,38 @@ class Walker {
     this.y = height / 2;
   }
   show() {
-  stroke(random(0, 255), random(0, 255), random(0, 255));
-  strokeWeight(35);;
-  fill(0)
-  point(this.x, this.y);
+  // noStroke();
+  stroke(1);
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  strokeWeight(1);
+  circle(this.x, this.y, 75);
   }
 
+  //favour downRight
   step() {
-    const choice = floor(random(4));
-    if (choice === 0) {
-      this.x++;
-    } else if (choice === 1){
-      this.x--;
-    } else if (choice === 2) {
-      this.y++;
-    } else if (choice === 3) {
-      this.y--;
+    let stuff = [-3, 0, 3];
+    let xStep = random(stuff);
+    let yStep = random(stuff);
+
+    const r = random(1)
+    if (r < 0.25) {
+      this.x += 3
+    } else if ( r < 0.5) {
+      this.x -= 3
+    } else if ( r < 0.75) {
+      this.y += 3
+    } else {
+      this.y -= 3
+    }
+    // this.x += xStep;
+    // this.y += yStep;
+  }
+
+  sing() {
+    const probability = 0.1;
+    const r = random(1);
+    if (r < probability) {
+      print("Sing!")
     }
   }
 }
